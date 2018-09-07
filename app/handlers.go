d6cfddeab08c336b168947e2918db02b0a3a15b1
bartr <bartr@microsoft.com>
2018-09-07 15:10:42 -0500

    added person structure

--- app/handlers.go
+++ app/handlers.go
@@ -5,6 +5,15 @@ import (
 	"net/http"
 )
 
+// this is the structure for the data portion of dc-receive messages
+type person struct {
+	DC    string `json:"DC"`
+	Items []struct {
+		FirstName string `json:"firstName"`
+		LastName  string `json:"lastName"`
+	} `json:"Items"`
+}
+
 type personHandler struct{}
 
 func (h personHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
