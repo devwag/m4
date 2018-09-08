3356a3d8e328414816218701e700ceb24b3abd25
bartr <bartr@microsoft.com>
2018-09-07 19:08:13 -0500

    updated event grid handler

--- eventgrid/eventgrid.go
+++ eventgrid/eventgrid.go
@@ -1,17 +1,55 @@
 package eventgrid
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
 // Handler - handle the event grid message
-func Handler(next http.Handler) http.Handler {
+func Handler(next func(w http.ResponseWriter, r *http.Request, env *Envelope)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Print("eventGridHandler ")
 
+		// only support post
+		// TODO - should we always check this?
+		if r.Method != "POST" {
+			w.WriteHeader(500)
+			log.Println(r.Method, " Not supported")
+		}
+
+		// TODO - should we add an https check?
+
+		var env Envelope
+
+		// decode the event grid message from the body
+		if r.Body != nil {
+			err := json.NewDecoder(r.Body).Decode(&env)
+			if err != nil {
+				w.WriteHeader(500)
+				log.Println(err)
+				return
+			}
+			r.Body.Close()
+		}
+
+		// verify event grid ID
+		if env.ID == "" {
+			w.WriteHeader(500)
+			log.Println("Event Grid Envelope: missing ID")
+			return
+		}
+
+		// verify event grid has data
+		// TODO - should we do this? are empty data messages possible?
+		if env.Data == nil {
+			w.WriteHeader(500)
+			log.Println("Event Grid Envelope: missing Data")
+			return
+		}
+
+		// call the next handler
 		if next != nil {
-			next.ServeHTTP(w, r)
+			next(w, r, &env)
 		}
 	})
 }
