93ad07bdf13e75cbbd1c86939f8d5585a0b87994
bartr <bartr@microsoft.com>
2018-09-08 15:17:34 -0500

    updated person handler

--- sampleapp/handlers.go
+++ sampleapp/handlers.go
@@ -2,28 +2,36 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"m4/eventgrid"
 	"net/http"
 )
 
-// this is the structure for the data portion of dc-receive messages
+// this is the structure for the data portion of event grid messages
 type person struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 }
 
+// handle event grid "person" messages
+// w and r are standard http.Handler params
+// env is the event grid envelope that was parsed by the eveng grid handler
 func personHandler(w http.ResponseWriter, r *http.Request, env *eventgrid.Envelope) {
 
+	// get the values from env.Data
 	var p person
 	err := json.Unmarshal(env.Data, &p)
 
 	if err == nil {
 		w.WriteHeader(200)
-		fmt.Fprintln(w, "person Handler: ", p.FirstName, "", p.LastName)
-		log.Println("person Handler: ", env.ID, p.FirstName, "", p.LastName)
+
+		// event grid doesn't inspect the body on a 200
+
+		log.Println("person Handler: ", env.ID, p.FirstName, p.LastName)
+
+		// TODO this is where you would process the "person message"
 	} else {
-		fmt.Println(err)
+		w.WriteHeader(500)
+		log.Println("ERROR:", err)
 	}
 }
