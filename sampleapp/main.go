a27f4d74ed3d051dd89cc9a704fef6191c0ad457
bartr <bartr@microsoft.com>
2018-09-08 14:49:58 -0500

    updated log handling

--- sampleapp/main.go
+++ sampleapp/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io"
 	"log"
+
+	// TODO should change these to github.com/bartr/m4 once stable
 	"m4/eventgrid"
 	"m4/logb"
 	"net/http"
@@ -19,9 +20,9 @@ import (
 // application startup
 func main() {
 
+	// parse command line flags
 	port := flag.Int("port", 8080, "TCP port to listen on")
 	logPath := flag.String("logpath", "/home/LogFiles/", "path to write log files")
-
 	flag.Parse()
 
 	setupLog(logPath)
@@ -31,13 +32,21 @@ func main() {
 
 func runServer(port int) {
 
+	// TODO - use a Go routine and capture ctrl c
+
+	// use gorilla mux
 	r := mux.NewRouter()
-	// r.Methods("POST")
+	r.Methods("POST")
+
+	// TODO make sure the request used https
 	// r.Headers("x-forwarded-proto", "https")
 
+	// this is our only handler
+	// chain the handlers together as middleware
 	r.Handle("/person", logb.Handler(eventgrid.Handler(personHandler)))
 	http.Handle("/", r)
 
+	// setup the server
 	srv := &http.Server{
 		Handler:      r,
 		Addr:         ":" + strconv.Itoa(port),
@@ -47,26 +56,55 @@ func runServer(port int) {
 
 	log.Println("Listening on port: ", port)
 
+	// this will block until the server ends
 	log.Println(srv.ListenAndServe())
 
 }
 
+// setup log multi writer
 func setupLog(logPath *string) {
-	// TODO - mkdir -p logFilePath
-	// TODO - add instance id to file name
 
-	if !strings.HasSuffix(*logPath, "/") {
-		*logPath += "/"
-	}
+	// prepend date and time to log entries
+	log.SetFlags(log.Ldate | log.Ltime)
 
-	logFile, err := os.OpenFile(*logPath+"app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	// open the log file
+	logFile, err := openLogFile(logPath)
 
+	// we ignore the open error which means everything gets logged to stdout
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	} else {
+		// setup a multiwriter to log to file and stdout
 		wrt := io.MultiWriter(os.Stdout, logFile)
 		log.SetOutput(wrt)
-		log.SetFlags(log.Ldate | log.Ltime)
 		log.Println("init complete")
 	}
+}
+
+// Open the log file
+func openLogFile(logPath *string) (*os.File, error) {
+	fileName := *logPath
+
+	if !strings.HasSuffix(fileName, "/") {
+		fileName += "/"
+	}
+
+	// make the log directory if it doesn't exist
+	err := os.MkdirAll(fileName, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	fileName += "app"
+
+	// use instance ID to differentiate log files between instances in App Services
+	iid := os.Getenv("WEBSITE_ROLE_INSTANCE_ID")
+	if iid != "" {
+		fileName += "_" + strings.TrimSpace(iid)
+	}
+
+	fileName += ".log"
+
+	// open the log file
+	return os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 }
--- sampleapp/main_test.go
+++ sampleapp/main_test.go
@@ -2,24 +2,38 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"m4/eventgrid"
 	"m4/logb"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestLogb(t *testing.T) {
 
-	s := `{"id":"1001","topic":"","subject":"person","eventType":"person","eventTime":"2018-08-20T18:04:26Z", "dataVersion": "1.0","data":{"firstName": "John", "LastName": "Doe"}}`
+	//	s := `{"id":"1001","topic":"","subject":"person","eventType":"person","eventTime":"2018-08-20T18:04:26Z", "dataVersion": "1.0","data":{"firstName": "John", "LastName": "Doe"}}`
 
-	// var env eventgrid.Envelope
-	// err := json.Unmarshal([]byte(s), &env)
+	// TODO use Envelope to build the json
+	env := eventgrid.Envelope{Subject: "person", EventType: "person", DataVersion: "1.0"}
+	env.ID = "1001"
+	env.EventTime = time.Now().UTC().Format("2006-01-02T15:04:05Z")
 
-	// var p person
-	// json.Unmarshal(env.Data, &p)
+	p := person{FirstName: "John", LastName: "Doe"}
+	json.Unmarshal(env.Data, &p)
 
-	r, err := http.NewRequest("POST", "https://www.logb.com/person", bytes.NewBufferString(s))
+	var b []byte
+	err := json.Unmarshal(b, &env)
+
+	fmt.Println(err)
+
+	fmt.Println(string(b))
+
+	return
+
+	r, err := http.NewRequest("POST", "https://www.logb.com/person", bytes.NewBufferString(string(b))) // bytes.NewBufferString(s))
 	if err != nil {
 		t.Error("NewRequest: ", err)
 	}
